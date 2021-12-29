84533b52218288f8cc87c022f7af01b2d9ca268f
Triyanafadila <60335009+Triyanafadila@users.noreply.github.com>
2021-12-30 02:18:06 +0700

    Update EmptyCart.js
    styling text

--- src/components/carts/EmptyCart.js
+++ src/components/carts/EmptyCart.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 function EmptyCart(props) {
     return (
-        <div>
-            your cart is currently empty
+     <div className="container mt-5">
+      <div className="row">
+        <div className="col-10 mx-auto text-center text-title text-capitalize">
+          <h1>your cart is currently empty</h1>
         </div>
+      </div>
+    </div>
     );
 }
 
